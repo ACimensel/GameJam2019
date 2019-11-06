007a75f42daf2698da5107eaca5b6c3618fc9482
ACimensel <cimensel.a@hotmail.com>
2019-11-05 22:02:15 -0800

    Extra assets + organize folders
    Added lots of extra assets, and organized the folders to store things

---
+++ Game Jam 2019 Project/Assets/Scripts/CarSteering.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSteering : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	[SerializeField]
+	private float accelerationPower = 100f;
+	[SerializeField]
+    private float steeringPower = 2f;
+    private float throttle, steer;
+
+    // Use this for initialization
+    void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        steer = Input.GetAxis ("Horizontal");
+        throttle = Input.GetAxis ("Vertical");
+
+        if (throttle>0) {
+            transform.Rotate(Vector3.up * steer * Mathf.Abs(steer) * steeringPower * throttle);
+        }
+        else if (throttle<0) {
+            transform.Rotate(Vector3.up * steer * Mathf.Abs(steer) * steeringPower * throttle);
+        }
+
+        rb.AddRelativeForce(Vector3.forward * throttle * accelerationPower);//moves car forward
+		//rb.AddRelativeForce( - Vector3.right * rb.velocity.magnitude * steeringAmount / 2);//moves car sideways
+			
+	}
+
+
+}
